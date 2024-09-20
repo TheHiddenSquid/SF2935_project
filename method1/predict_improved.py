616de02e06c0bd8b96f706265657992f0eb2e9de
Ismael Parada <ismaelp@kth.se>
2024-09-20 12:26:39 +0200

    added kde

--- method1/predict_improved.py
+++ method1/predict_improved.py
@@ -42,7 +42,7 @@ def generate_pdfs(songs):
     disliked_pdfs = {}
 
     # Beta distribution for all these:
-    attributes = ["danceability", "energy", "loudness", "speechiness", "acousticness", "liveness", "valence","tempo", "instrumentalness"]
+    attributes = ["danceability", "energy", "speechiness", "acousticness", "liveness", "instrumentalness"]
     bounds_dict = {"danceability":[0,1], "energy":[0,1],"loudness":[-60,0], "speechiness":[0,1], "acousticness":[0,1], "liveness":[0,1], "valence":[0,1], "tempo":[20,200], "instrumentalness":[0,1]}
 
     for attr in attributes:
@@ -53,14 +53,25 @@ def generate_pdfs(songs):
 
         a,b, *args = stats.fit(stats.beta, liked_data, [(0, 100), (0, 100)]).params
         liked_pdfs[attr] = stats.beta(a, b)
-        #liked_pdfs[attr] = stats.gaussian_kde(liked_data)
        
         a,b, *args = stats.fit(stats.beta, disliked_data, [(0, 100), (0, 100)]).params
         disliked_pdfs[attr] = stats.beta(a, b)
-        #liked_pdfs[attr] = stats.gaussian_kde(disliked_data)
     
 
-    # Custum for "mode"
+    # Kernel density estimation for these:
+    attributes = ["loudness", "valence", "tempo"]
+
+    for attr in attributes:
+        m = bounds_dict[attr][0]    # For rescaling to [0,1]
+        M = bounds_dict[attr][1]    # For rescaling to [0,1]
+        liked_data = [(getattr(x, attr)-m)/(M-m) for x in liked_songs]
+        disliked_data = [(getattr(x, attr)-m)/(M-m) for x in disliked_songs]
+
+        liked_pdfs[attr] = stats.gaussian_kde(liked_data)
+        disliked_pdfs[attr] = stats.gaussian_kde(disliked_data)
+
+
+    # Categotical for "mode"
     attr = "mode"
     liked_data = [getattr(x, attr) for x in liked_songs]
     disliked_data = [getattr(x, attr) for x in disliked_songs]
@@ -69,7 +80,7 @@ def generate_pdfs(songs):
     disliked_pdfs[attr] = [disliked_data.count(x)/len(disliked_data) for x in [0,1]]
 
 
-    # Custom for "key"
+    # Categotical for "key"
     attr = "key"
     liked_data = [getattr(x, attr) for x in liked_songs]
     disliked_data = [getattr(x, attr) for x in disliked_songs]
@@ -85,7 +96,7 @@ def eval_pdf(song, pdfs, epsilon=0.01):
     tot = 1
 
     # Count all beta distributions:
-    attributes = ["danceability", "energy", "loudness", "speechiness", "acousticness", "liveness", "valence","tempo"]
+    attributes = ["danceability", "energy", "speechiness", "acousticness", "liveness"]
     bounds_dict = {"danceability":[0,1], "energy":[0,1], "loudness":[-60,0], "speechiness":[0,1], "acousticness":[0,1], "liveness":[0,1], "valence":[0,1], "tempo":[20,200]}
 
     for attr in attributes:
@@ -100,6 +111,21 @@ def eval_pdf(song, pdfs, epsilon=0.01):
             tot *= pdfs[attr].cdf(rel_x+epsilon/2) - pdfs[attr].cdf(rel_x-epsilon/2)
 
 
+    # Count the Kernel density estimations:
+    attributes = ["loudness","valence","tempo"]
+
+    for attr in attributes:
+        m = bounds_dict[attr][0]
+        M = bounds_dict[attr][1]
+        rel_x = (getattr(song, attr)-m)/(M-m)
+        if rel_x - epsilon/2 < 0:
+            tot *= pdfs[attr].integrate_box_1d(0, rel_x+epsilon)
+        elif rel_x + epsilon/2 > 1:
+            tot *= pdfs[attr].integrate_box_1d(rel_x-epsilon, 1)
+        else:
+            tot *= pdfs[attr].integrate_box_1d(rel_x-epsilon/2, rel_x+epsilon/2)
+
+
     # Count the categorical-distributions:
     attr = "mode"
     tot *= pdfs[attr][getattr(song, attr)]
@@ -107,7 +133,7 @@ def eval_pdf(song, pdfs, epsilon=0.01):
     attr = "key"
     tot *= pdfs[attr][getattr(song, attr)]
 
-    # Custom for instrumentalness:
+    #Custom for instrumentalness:
     attr = "instrumentalness"
     rel_x = getattr(song, attr)
     if rel_x < 0.01:
@@ -153,10 +179,10 @@ def test(songs, no_tests):
 
 
 def main():
-    random.seed("rando1m")
+    random.seed("ra1ndom")
     songs = get_songs()
    
-    print(test(songs, no_tests=20))
+    print(test(songs, no_tests=25))
    
 
 if __name__ == "__main__":
